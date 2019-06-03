46169cfd07f2efb1d12336128c268da10fbe16d6
VelzA <36815970+expectoviz@users.noreply.github.com>
2019-06-03 14:47:30 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "
+  ___          ____    _    __      _    
+ | __|_ ___ __|__ / __| |_ /  \__ _| |___
+ | _|\ \ / '_ \|_ \/ _|  _| () \ V /_|_ /
+ |___/_\_\ .__/___/\__|\__|\__/ \_/(_)__|
+         |_|                             
+        
+    Visit : https://cybergd-article.gq
+        "
+        
+read -p "Webdav target : " url
+read -p "Select file to upload " path;
+if [curl -T /$path $url
+echo "Result : " $url/$path
